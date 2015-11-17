0e319142c1ac096acd4ea393c4b12d7337593a71
Lance Ball <lball@redhat.com>
2015-11-17 07:42:55 -0500

    Rework the whole thing to actually conform to the spec (and pass 879 tests)

--- test/simple.js
+++ test/simple.js
@@ -5,74 +5,9 @@ var assert = require('assert'),
 describe('A fidelity promise', function() {
 
   it('should begin life in a PENDING state', function(done) {
-    var p = promise(function() { });
+    var p = promise();
     assert.equal(promise.PENDING, p.state);
     done();
-  });
-
-  describe('done()', function() {
-    it('should resolve the value', function (done) {
-      var p = promise(function(resolve, reject) {
-        resolve('testDone resolution value');
-      });
-
-      p.done(function(value) {
-        assert.equal(value, 'testDone resolution value');
-        done();
-      });
-    });
-
-    it('should set successfuylly completed promise state to FULFILLED', function (done) {
-      var p = promise(function(resolve, reject) {
-        resolve('testDone resolution value');
-      });
-
-      p.done(function(value) {
-        assert.equal(promise.FULFILLED, p.state);
-        done();
-      });
-    });
-
-    it('should resolve the value asynchronously', function(done) {
-      var p = promise(function(resolve, reject) {
-        setImmediate(function() {
-          resolve('testDone resolution value');
-        });
-      });
-
-      p.done(function(value) {
-        assert.equal(value, 'testDone resolution value');
-        assert.equal(promise.FULFILLED, p.state);
-        done();
-      });
-    });
-
-    it('should give a reason for failed tasks', function(done) {
-      var p = promise(function(resolve, reject) {
-        setImmediate(function() {
-          reject('You smell funny');
-        });
-      });
-
-      p.done(function(value) {
-        assert.equal(value, null);
-      },
-      function(reason) {
-        assert.equal(reason, 'You smell funny');
-        done();
-      });
-    });
-
-    it('should set a failed promise state to REJECTED', function(done) {
-      var p = promise(function(resolve, reject) {
-        setImmediate(function() {
-          reject('You smell funny');
-        });
-      });
-
-      p.done(function(value) { assert.equal(value, null); },
-             function(reason) { assert.equal(promise.REJECTED, p.state); done();});
-    });
   });
 
   describe('then()', function() {
@@ -140,17 +75,13 @@ describe('A fidelity promise', function() {
       var dummy = { dummy: "dummy" };
 
       d.promise.then(function onFulfilled() {
-        console.log("THEN CALLED " + isFulfilled);
         assert.strictEqual(isFulfilled, true);
         done();
       });
 
       setTimeout(function () {
-        console.log('TIMEOUT CALLED');
         d.resolve(dummy);
-        console.log('RESOLUTION DONE');
         isFulfilled = true;
-        console.log('ISFULFILLED TRUE');
       }, 50);
     });
   });
@@ -159,22 +90,16 @@ describe('A fidelity promise', function() {
 
 
 function testChainedPromises() {
-  console.log('testChainedPromises');
   var p = promise(function(resolve, reject) {
-    console.log('first promise resolving');
     resolve('First resolved value');
   });
 
   p.then(function(value) {
-    console.log('first promise complete');
     assert.equal(value, 'First resolved value');
     return promise(function(resolve) {
-      console.log('second promise resolving');
       resolve('Second resolved value');
     });
   }).then(function(value) {
-    console.log('second promise complete');
     assert.equal(value, 'Second resolved value');
-    console.log('testChainedPromises complete');
   });
 }
