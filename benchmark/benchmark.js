6b06a5f3985e538335bff5e4a7071e6c5ba7a078
Lance Ball <lball@redhat.com>
2016-09-07 11:16:55 -0400

    Move to genet for profiling output

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,73 +1,33 @@
 'use strict';
 
+/**
+ * These are the packages we are testing against
+ */
 const Fidelity = require('../lib/index.js');
 const Bluebird = require('bluebird');
 const PromiseModule = require('promise');
 const Q = require('q');
-const profiler = require('v8-profiler');
-const fs = require('fs');
 
-let profilerRunning = false;
-
-function toggleProfiling () {
-  if (profilerRunning) {
-    const profile = profiler.stopProfiling();
-    console.log('stopped profiling');
-    profile.export()
-      .pipe(fs.createWriteStream('./fidelity-' + Date.now() + '.cpuprofile'))
-      .once('error', profiler.deleteAllProfiles)
-      .once('finish', profiler.deleteAllProfiles);
-    profilerRunning = false;
-    return;
-  }
-  profiler.startProfiling();
-  profilerRunning = true;
-  console.log('started profiling');
-}
-
-console.log('PID', process.pid);
-process.on('SIGUSR2', toggleProfiling);
-
-function getRandomInt (min, max, callback) {
-  setImmediate(() => callback(Math.floor(Math.random() * (max - min)) + min));
-}
-
-function newNativePromise () {
-  return new Promise((resolve, reject) => getRandomInt(0, 10, resolve));
-}
-
-function newFidelityPromise () {
-  return new Fidelity((resolve, reject) => getRandomInt(0,10, resolve));
-}
-
-function newBluebirdPromise () {
-  return new Bluebird((resolve, reject) => getRandomInt(0,10, resolve));
-}
-
-function newQPromise () {
-  return new Q.promise((resolve, reject) => getRandomInt(0,10, resolve));
-}
-
-function newPromiseModulePromise () {
-  return new PromiseModule((resolve, reject) => getRandomInt(0,10, resolve));
-}
+const someWork = (resolve) => {
+  setImmediate(() => resolve(Math.floor(Math.random() * (10 - 0))));
+};
 
 function runBenchmarks () {
   exports.compare = {
-    "new native Promise" : function (done) {
-      newNativePromise().then(done);
+    'new Fidelity Promise': function (done) {
+      new Fidelity(someWork).then(done);
     },
-    "new Fidelity Promise" : function(done) {
-      newFidelityPromise().then(done);
+    'new native Promise': function (done) {
+      new Promise(someWork).then(done);
     },
-    "new Bluebird Promise" : function (done) {
-      newBluebirdPromise().then(done);
+    'new Bluebird Promise': function (done) {
+      new Bluebird(someWork).then(done);
     },
-    "new QPromise" : function (done) {
-      newQPromise().then(done);
+    'new QPromise': function (done) {
+      new Q.promise(someWork).then(done);
     },
-    "new PromiseModule()" : function (done) {
-      newPromiseModulePromise().then(done);
+    'new PromiseModule()': function (done) {
+      new PromiseModule(someWork).then(done);
     }
   };
 
@@ -76,5 +36,5 @@ function runBenchmarks () {
   exports.compareCount = 10;
   require('bench').runMain();
 }
-runBenchmarks();
 
+runBenchmarks();
---
+++ benchmark/profile.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const Fidelity = require('../lib/index.js');
+const Genet = require('genet');
+
+const someWork = (resolve) => {
+  setImmediate(() => resolve(Math.floor(Math.random() * (10 - 0))));
+};
+
+function runBenchmarks () {
+  const profile = new Genet({
+    profileName: 'fidelity',
+    // filter out everything but fidelity core code
+    filter: /^(?!.*benchmark)(?=.*fidelity).*/,
+    duration: 50000,
+    showAppOnly: true,
+    verbose: true,
+    flamegraph: true
+  });
+
+  exports.compare = {
+    'new Fidelity Promise': function (done) {
+      new Fidelity(someWork).then(done);
+    },
+    'Fidelity.resolve()': function (done) {
+      Fidelity.resolve(Math.floor(Math.random() * (10 - 0))).then(done);
+    }
+  };
+
+  let num = 1;
+  exports.done = function (data) {
+    profile.stop().then(() => console.log('Profiling stopped'));
+    bench.show(data);
+    console.error('done', num);
+    num = num + 1;
+  };
+
+  exports.time = 5000;
+  exports.countPerLap = 6;
+  exports.compareCount = 8;
+
+  profile.start();
+
+  const bench = require('bench');
+  bench.runMain();
+}
+
+runBenchmarks();
